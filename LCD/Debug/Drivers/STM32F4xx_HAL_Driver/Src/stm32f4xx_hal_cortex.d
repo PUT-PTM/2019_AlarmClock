3a83344b3e6e6fa746d98765a83da673e7d173c3
stasiu9 <46819190+stasiu9@users.noreply.github.com>
2019-04-01 16:42:08 +0200

    project

---
+++ LCD/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Inc/stm32f4xx_hal_conf.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Inc/main.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Inc/stm32f4xx_hal_conf.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Inc/main.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/Users/stanislawkucznerowicz/Documents/Ptm/LCDDobre/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
--- LCD/Src/main.c
+++ LCD/Src/main.c
@@ -39,13 +39,16 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f4xx_hal.h"
-#include "i2c-lcd.h"
+#include "ds3231.h"
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
+
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
@@ -56,6 +59,8 @@ I2C_HandleTypeDef hi2c1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+static void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -76,7 +81,7 @@ static void MX_I2C1_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	uint8_t str1[]="USART Transmit\r\n";
   /* USER CODE END 1 */
 
   /* MCU Configuration----------------------------------------------------------*/
@@ -98,25 +103,43 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_I2C2_Init();
+  MX_USART2_UART_Init();
+  RTC_Init();
   /* USER CODE BEGIN 2 */
   lcd_init ();
-  int i=150;
+  int trzy=3;
 
+
+
+  HAL_UART_Transmit(&huart2,str1,16,0xFFFF);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  lcd_send_cmd (0x80);  // goto 1,1
+	  //getTimeAndDate();
 
-	 	  lcd_send_integer(i,3);
 
-	 	  lcd_send_cmd (0xc0);
 
-	 	  lcd_send_string ("wiecej Ogarnr");
-	 	  HAL_Delay(1000);
-	  i++;
+	  //int sekundy=DS3231_Time.seconds;
+
+	 lcd_send_cmd (0x80);  // goto 1,1
+
+	 lcd_send_integer(trzy,2);
+
+
+
+	 //lcd_send_cmd(0xc0);
+	 //lcd_send_string("Czas Europejski");
+
+
+	 HAL_Delay(2000);
+
+
+
+
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
@@ -211,6 +234,45 @@ static void MX_I2C1_Init(void)
 
 }
 
+/* I2C2 init function */
+static void MX_I2C2_Init(void)
+{
+
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART2 init function */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
 /** Configure pins as 
         * Analog 
         * Input 
@@ -223,6 +285,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
 }
